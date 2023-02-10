703e0e128752008a597a62413e5bfdda8083afab
neezar <rforlyffe@gmail.com>
2023-02-10 22:38:47 +0800

    clean up

--- src/env/schema.mjs
+++ src/env/schema.mjs
@@ -23,7 +23,7 @@ export const serverEnv = {
  * To expose them to the client, prefix them with `NEXT_PUBLIC_`.
  */
 export const clientSchema = z.object({
-  // NEXT_PUBLIC_CLIENTVAR: z.string(),
+  NEXT_PUBLIC_CLIENTVAR: z.string(),
 });
 
 /**
@@ -33,5 +33,5 @@ export const clientSchema = z.object({
  * @type {{ [k in keyof z.input<typeof clientSchema>]: string | undefined }}
  */
 export const clientEnv = {
-  // NEXT_PUBLIC_CLIENTVAR: process.env.NEXT_PUBLIC_CLIENTVAR,
+  NEXT_PUBLIC_CLIENTVAR: process.env.NEXT_PUBLIC_CLIENTVAR,
 };
